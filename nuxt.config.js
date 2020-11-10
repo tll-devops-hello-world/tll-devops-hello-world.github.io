b4053143ff06ed08437800f336ed0c0586986928
elaynelemos <elayne.l.lemos@gmail.com>
2020-11-09 23:35:32 -0300

    Fixes deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,12 @@ export default {
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/'
+  },
+  generate: {
+    fallback: true
+  }
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
